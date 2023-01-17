ec379e48be24a930c4d37399ad21cd8ae8df89b5
Profesor Vespertino <ProfVespertino@PROFESOR06.informatica.comercio>
2023-01-17 21:40:35 +0100

    Fix js

--- Tema_9/Pets/js/registro.js
+++ Tema_9/Pets/js/registro.js
@@ -3,52 +3,54 @@ Tags.init();
 
 document.addEventListener("DOMContentLoaded", async function () {
   // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  const forms = document.querySelectorAll('.needs-validation')
+  const form = document.querySelector('#form')
 
-  // Loop over them and prevent submission
-  Array.from(forms).forEach(form => {
-    form.addEventListener('submit', async event => {
+  form.addEventListener('submit', async (event) => {
 
-      if (!form.checkValidity()) {
-        event.preventDefault()
-        event.stopPropagation()
-      } else {
-        const categoria = document.querySelector("#validationCustom01").value;
-        const dogName = document.querySelector("#validationCustom02").value;
-        const tags = "noTags jaja"
+    form.classList.add('was-validated')
+    event.preventDefault()
+    event.stopPropagation()
 
-        const pet = {
-          category: {
-            name: categoria
-          },
-          name: dogName,
-          tags: [
-            {
-              name: tags
-            }
-          ],
-          status: "available"
-        };
-        const data = await postData('https://petstore.swagger.io/v2/pet', pet)
-        console.log(data);
-      }
+    if (!form.checkValidity()) {
+      return;
+    }
+    const categoria = document.querySelector("#validationCustom01").value;
+    const dogName = document.querySelector("#validationCustom02").value;
+    const tags = "noTags jaja"
 
-      form.classList.add('was-validated')
-    }, false)
-
-  })
+    const pet = {
+      category: {
+        name: categoria
+      },
+      name: dogName,
+      tags: [
+        {
+          name: tags
+        }
+      ],
+      status: "available"
+    };
+    const data = await postData('https://petstore.swagger.io/v2/pet', pet);
+    console.log(data);
+    form.submit();
+  }, false);
 });
 
 // Ejemplo implementando el metodo POST:
 async function postData(url = '', data = {}) {
-  const response = await fetch(url, {
+  await fetch(url, {
     method: 'POST',
-    mode: "no-cors",
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+      'accept': 'application/json'
     },
     body: JSON.stringify(data)
-  });
-  return response.json(); // parses JSON response into native JavaScript objects
+  }).catch(error => {
+    {
+      new bootstrap.Modal(document.getElementById('exampleModal')).show();
+      console.log(error)
+    }
+  })
+    .then(response => response.json());
 }
 
