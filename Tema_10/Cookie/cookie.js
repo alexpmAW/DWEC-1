ce6b975b283e8b5fa4bf7de6afd11489901790bb
Your Name <you@example.com>
2023-01-26 23:07:17 +0100

    Fix Ejemplo cookie, TODO IndexDB

--- Tema_10/Cookie/cookie.js
+++ Tema_10/Cookie/cookie.js
@@ -1,21 +1,6 @@
 
-document.addEventListener("DOMContentLoaded", function () {
-    document.querySelector("#create").addEventListener("click", function () {
-        createCookie("defaultCookie", document.querySelector("#createInput").value);
-    })
-    document.querySelector("#view").addEventListener("click", function () {
-        document.querySelector("#viewInput").value = readCookie("defaultCookie");
-    })
-    document.querySelector("#modify").addEventListener("click", function () {
-        updateCookie("defaultCookie", document.querySelector("#modifyInput").value);
-    })
-    document.querySelector("#delete").addEventListener("click", function () {
-        deleteCookie("defaultCookie");
-    })
-});
-
 // Function to create a cookie
-function createCookie(name, value, days) {
+export function createCookie(name, value, days) {
     var expires;
     if (days) {
         const date = new Date();
@@ -29,7 +14,7 @@ function createCookie(name, value, days) {
 }
 
 // Function to read a cookie
-function readCookie(name) {
+export function readCookie(name) {
     let nameEQ = name + "=";
     const ca = document.cookie.split(';');
     for (let i = 0; i < ca.length; i++) {
@@ -41,11 +26,11 @@ function readCookie(name) {
 }
 
 // Function to update a cookie
-function updateCookie(name, value, days) {
+export function updateCookie(name, value, days) {
     createCookie(name, value, days);
 }
 
 // Function to delete a cookie
-function deleteCookie(name) {
+export function deleteCookie(name) {
     createCookie(name, "", -1);
 }
