cd40a5e90e3600d9bf580c30b5a9c3c89f1bd39c
Profesor Vespertino <ProfVespertino@PROFESOR06.informatica.comercio>
2023-02-03 21:47:11 +0100

    Fix js

--- Tema_10/Redis/app.js
+++ Tema_10/Redis/app.js
@@ -1,28 +1,19 @@
-import { createServer } from 'http'
 import { createClient } from 'redis';
+import { readFile } from 'fs/promises';
 
-
-const hostname = '127.0.0.1'
-const port = 3000
-
-const server = createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    res.end('Hello World\n')
-})
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`)
-})
+const json = JSON.parse(
+    await readFile(
+        new URL('./file.json', import.meta.url)
+    )
+);
 
 const client = createClient({
-    url: 'redis://alice:foobared@awesome.redis.server:6380'
+    url: 'redis://default:123pass@localhost:5000'
 });
 
 client.on('error', err => console.log('Redis Client Error', err));
-
 await client.connect();
+await client.set('nuevaClave', JSON.stringify(json));
+const value = await client.get('HOLA');
 
-await client.set('key', 'value');
-const value = await client.get('key');
 await client.disconnect();
