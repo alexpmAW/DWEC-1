6c0dc0bafa6354644a92322e15cf7e5c70b2f66d
Your Name <you@example.com>
2023-02-07 15:25:01 +0100

    Add rest API

--- Tema_10/Redis/app.js
+++ Tema_10/Redis/app.js
@@ -1,5 +1,8 @@
 import { createClient } from 'redis';
 import { readFile } from 'fs/promises';
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const json = JSON.parse(
     await readFile(
@@ -7,6 +10,7 @@ const json = JSON.parse(
     )
 );
 
+/*
 const client = createClient({
     url: 'redis://default:123pass@localhost:5000'
 });
@@ -17,3 +21,79 @@ await client.set('nuevaClave', JSON.stringify(json));
 const value = await client.get('HOLA');
 
 await client.disconnect();
+*/
+
+
+const app = express()
+const port = 3000
+
+let books = [];
+
+app.use(cors());
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/book', (req, res) => {
+    const book = req.body;
+
+    // Output the book to the console for debugging
+    console.log(book);
+    books.push(book);
+
+    res.send('Book is added to the database');
+});
+
+app.get('/books', (req, res) => {
+    res.json(books);
+});
+
+app.get('/book/:isbn', (req, res) => {
+    // Reading isbn from the URL
+    const isbn = req.params.isbn;
+
+    // Searching books for the isbn
+    for (let book of books) {
+        if (book.isbn === isbn) {
+            res.json(book);
+            return;
+        }
+    }
+
+    // Sending 404 when not found something is a good practice
+    res.status(404).send('Book not found');
+});
+
+app.delete('/book/:isbn', (req, res) => {
+    // Reading isbn from the URL
+    const isbn = req.params.isbn;
+
+    // Remove item from the books array
+    books = books.filter(i => {
+        if (i.isbn !== isbn) {
+            return true;
+        }
+        return false;
+    });
+
+    res.send('Book is deleted');
+});
+
+app.put('/book/:isbn', (req, res) => {
+    // Reading isbn from the URL
+    const isbn = req.params.isbn;
+    const newBook = req.body;
+
+    // Remove item from the books array
+    for (let i = 0; i < books.length; i++) {
+        let book = books[i]
+        if (book.isbn === isbn) {
+            books[i] = newBook;
+        }
+    }
+
+    res.send('Book is edited');
+});
+
+app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
