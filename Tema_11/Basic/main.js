bcaff445396512eaf65ab0a165f6e14576d59c51
Profesor Vespertino <ProfVespertino@PROFESOR06.informatica.comercio>
2023-02-21 16:37:10 +0100

    hack leaflet haha

--- Tema_11/Basic/main.js
+++ Tema_11/Basic/main.js
@@ -3,12 +3,26 @@ document.addEventListener("DOMContentLoaded", async function () {
 });
 
 function initMap() {
-    const mymap = L.map('mapid').setView([51.505, -0.09], 13);
+    const mymap = L.map('mapid').setView([0, 0], 13);
 
+    mymap.flyTo([42.46309052000472, -2.441003322601319], 8)
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
     }).addTo(mymap);
 
     L.marker([51.5, -0.09]).addTo(mymap)
         .bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
+
+    let lat, lng;
+
+    mymap.addEventListener('mousemove', function (ev) {
+        lat = ev.latlng.lat;
+        lng = ev.latlng.lng;
+    });
+
+    document.getElementById("mapid").addEventListener("contextmenu", function (event) {
+        event.preventDefault();
+        alert(lat + ' - ' + lng);
+        return false;
+    })
 }
